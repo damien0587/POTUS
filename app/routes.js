8415ea1c4eb197e0fda2427ab6522fb598eb1d28
damien0587 <damien0587@gmail.com>
2017-12-19 23:26:41 -0600

    auth

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,7 @@ module.exports = function(app, passport) {
     // =====================================
     // HOME PAGE (with login links) ========
     // =====================================
-    app.get('/', function(req, res) {
+    app.get('/loginsignup', function(req, res) {
         res.render('index.ejs'); // load the index.ejs file
     });
 
