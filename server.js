9a2d39848e40756d4600ed5871771a9d5ebf9d2a
damien0587 <damien0587@gmail.com>
2017-12-19 23:44:20 -0600

    authorization

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
 
 app.set('view engine', 'ejs'); // set up ejs for templating
-app.use(express.static(__dirname + '/startbootstrap-agency-gh-pages'));
+app.use(express.static(__dirname + '/fullpotspotz-gh-pages'));
 
 // required for passport
 app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
